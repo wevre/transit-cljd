a96304f92e1f1ae5290fea7a3d956471ecb77cd9
wevre <6877948+wevre@users.noreply.github.com>
2023-03-26 13:39:58 -0600

    initial commit

---
+++ bin/transit_cljd.dart
@@ -0,0 +1,1 @@
+export "../lib/cljd-out/wevre/transit-cljd.dart" show main;
---
+++ test/transit_cljd_test.dart
@@ -0,0 +1,8 @@
+import 'package:transit_cljd/transit_cljd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
